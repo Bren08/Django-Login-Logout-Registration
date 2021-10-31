7b8346dc63ab0b54859ef828e1880b752f79b579
Brendan O Brien <brenande@gmail.com>
2021-10-31 11:44:05 +0000

    modified models.py to include args

--- checkout/models.py
+++ checkout/models.py
@@ -19,7 +19,7 @@ class Order(models.Model):
 
 class OrderLineItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
-    product = models.ForeignKey(Aid, null=False)
+    product = models.ForeignKey(Aid, on_delete=models.CASCADE)
     quantity = models.IntegerField(blank=False)
 
     def __str__(self):
